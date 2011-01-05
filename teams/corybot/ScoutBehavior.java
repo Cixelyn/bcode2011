5f5ed2e4e7b1c7645cb502545a90e05c830c4eac
Cory Li <51144+Cixelyn@users.noreply.github.com>
2011-01-04 21:02:57 -0500

    Update with some more stuff

---
+++ teams/corybot/ScoutBehavior.java
@@ -0,0 +1,30 @@
+package corybot;
+
+import battlecode.common.ComponentController;
+
+public class ScoutBehavior extends Behavior{
+
+	public ScoutBehavior(RobotPlayer player) {
+		super(player);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void newComponentCallback(ComponentController[] components) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
