14dfb11ffaf3939717725ed866a00effa163907b
maxnelso <3494159+maxnelso@users.noreply.github.com>
2011-01-06 15:40:25 -0500

    Hello person who is reading this!

---
+++ teams/maxbot2/BuildingBehavior.java
@@ -0,0 +1,47 @@
+package maxbot2;
+
+import battlecode.common.*;
+
+public class BuildingBehavior extends Behavior {
+
+	public BuildingBehavior(RobotPlayer player) {
+		super(player);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void newComponentCallback(ComponentController[] components) {
+		
+	
+		//Switch to Refinery Behavior
+		if(Utility.hasComponent(ComponentType.RECYCLER,components)) {
+			if (runtime == 0) // Building started with recycler
+				myPlayer.swapBehavior(new MainRefineryBehavior(myPlayer));
+			else
+				myPlayer.swapBehavior(new ExpoRefineryBehavior(myPlayer));		
+		}
+		
+
+	}
+
+
+
+
+	public void run() throws Exception {
+		if (Clock.getRoundNum() <= 2)
+			myPlayer.swapBehavior(new MainRefineryBehavior(myPlayer));
+		else
+			myPlayer.swapBehavior(new ExpoRefineryBehavior(myPlayer));
+	}
+
+	public String toString() {
+		return "DefaultBuildingBehavior";
+	}
+
+	
+	public void newMessageCallback(Message msg) {
+		
+		
+	}
+
+}
