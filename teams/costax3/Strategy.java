f5481fb4d84dd33c839e59c2b2648d88c173863f
Cory Li <51144+Cixelyn@users.noreply.github.com>
2011-01-09 18:20:33 -0500

    Implemented Strategy Abstraction. Started fleshing out Memo Testing Framework

---
+++ teams/costax3/Strategy.java
@@ -0,0 +1,12 @@
+package costax3;
+import battlecode.common.*;
+
+/**
+ * A strategy is a collection of behaviors that are instantiated upon robot default.
+ * Strategies should only change _BETWEEN_ games.  Not during games
+ * @author Cory
+ *
+ */
+public abstract class Strategy {
+	public abstract Behavior selectBehavior(RobotPlayer player, int currTime);
+}
---
+++ teams/costax3/strategy/DefaultStrategy.java
@@ -0,0 +1,31 @@
+package costax3.strategy;
+import costax3.*;
+
+public class DefaultStrategy extends Strategy {
+
+	public Behavior selectBehavior(RobotPlayer player, int currTime) {
+		Behavior b = null;
+		switch(player.myRC.getChassis()) {
+		case BUILDING:
+			b = new BuildingBehavior(player);
+			break;
+		case LIGHT:
+			b = new LightBehavior(player);
+			break;
+		case MEDIUM:
+			b = new MediumBehavior(player);
+			break;
+		case HEAVY:
+			b = new HeavyBehavior(player);
+			break;
+		case FLYING:
+			b = new FlyingBehavior(player);
+			break;
+		default:
+			System.out.println("Error");
+		}
+		
+		return b;
+	}
+
+}
