c9c45013bc85ca37023904608be9a8e021342b6e
Justin Venezuela <jven@jvenezue.la>
2011-01-09 22:32:59 -0500

    Missed committing some stuff

---
+++ teams/costax4/behaviors/MarineBuildOrder.java
@@ -0,0 +1,6 @@
+package costax4.behaviors;
+
+public enum MarineBuildOrder {
+	EQUIPPING,
+	MOVE_OUT
+}
---
+++ teams/costax4/behaviors/RefineryBuildOrder.java
@@ -0,0 +1,12 @@
+package costax4.behaviors;
+
+public enum RefineryBuildOrder 
+{
+	EQUIPPING,
+	GIVE_ANTENNA,
+	WAITING,
+	MAKE_MULE,
+	EQUIP_MULE,
+	MAKE_MARINE,
+	EQUIP_MARINE
+}
