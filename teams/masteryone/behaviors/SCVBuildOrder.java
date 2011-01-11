99cc32ea22e35c5c6542d812b91009cfbf9c259e
Justin Venezuela <jven@jvenezue.la>
2011-01-10 19:34:06 -0500

    SCV builds factory and armory

--- teams/masteryone/behaviors/SCVBuildOrder.java
+++ teams/masteryone/behaviors/SCVBuildOrder.java
@@ -3,9 +3,11 @@ package masteryone.behaviors;
 public enum SCVBuildOrder 
 {
 	FIND_MINE,
-	CAP_MINE,
-	ADDON_MINE,
+	BUILD_REFINERY,
 	BUILD_ARMORY,
+	VACATE_HOME,
+	VACATE_FACTORY,
+	BUILD_FACTORY,
 	WAITING,
 	WEIRD_SPAWN	
 }
