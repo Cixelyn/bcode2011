11dcf7c3f46497ebef87b3cf3ac69321536900f4
Cory Li <51144+Cixelyn@users.noreply.github.com>
2011-01-16 02:06:20 -0500

    Map sensing added (currently untested)

--- teams/suboptimal/behaviors/TestHeavyBehavior.java
+++ teams/suboptimal/behaviors/TestHeavyBehavior.java
@@ -26,6 +26,15 @@ public class TestHeavyBehavior extends Behavior
 	public TestHeavyBehavior(RobotPlayer player)
 	{
 		super(player);
+		
+		
+		//lets initialize our map navigation variables.
+		mapLeftEdge = myPlayer.myBirthplace.x;
+		mapRightEdge = myPlayer.myBirthplace.x;
+		mapBottomEdge = myPlayer.myBirthplace.y;
+		mapTopEdge = myPlayer.myBirthplace.y;
+		
+			
 	}
 
 
@@ -58,7 +67,9 @@ public class TestHeavyBehavior extends Behavior
 					obj = TestHeavyBuildOrder.MOVE_OUT;
 				return;
 	        	
+				
 			case MOVE_OUT:	
+				
 				
 	        	myPlayer.myRC.setIndicatorString(1, "MOVE_OUT");
 	        	Utility.bounceNav(myPlayer);
@@ -69,12 +80,120 @@ public class TestHeavyBehavior extends Behavior
 	
 	
 	
-	public String toString()
-	{
-		return "TestHeavyBehavior";
+	
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////
+	///////////// MAP POSITION ESTIMATION CODE /////////////////////////////////////////
+	
+	
+	//Booleans whether edges are ofund
+	boolean foundTopEdge = false;
+	boolean foundRightEdge = false;
+	boolean foundBottomEdge = false;
+	boolean foundLeftEdge = false;
+	
+	//Edges of the map (or farthest location if edges haven't been found)
+	int mapLeftEdge;
+	int mapRightEdge;
+	int mapBottomEdge;
+	int mapTopEdge;
+	
+	
+	//Quick constant here for some faster access
+	private static final int SATRANGE = ComponentType.SATELLITE.range;
+	
+	
+	
+	/**
+	 * 
+	 * We only need to scan for edges that we have yet to detect.
+	 * This code is optimized for and only works w/ satellites
+	 * 
+	 * Map edges are scanned, and results are stored into the
+	 * mapXXX,foundXXX variables.
+	 *  
+	 */
+	public void satelliteScanMapEdge() {
+		
+		MapLocation toScan;
+		MapLocation currLoc = myPlayer.myRC.getLocation();
+		int myX = currLoc.x;
+		int myY = currLoc.y;
+		
+		if(!foundTopEdge) {
+			toScan = new MapLocation(myX,myY-SATRANGE);
+			if(myPlayer.myRC.senseTerrainTile(toScan)==TerrainTile.OFF_MAP) {
+				foundTopEdge = true;
+				mapTopEdge = scanForLand(toScan,0,-1).y;
+			} else {
+				if(myY<mapTopEdge) mapTopEdge=myY;
+			}
+		}
+		if(!foundRightEdge) {
+			toScan = new MapLocation(myX+SATRANGE,myY);
+			if(myPlayer.myRC.senseTerrainTile(toScan)==TerrainTile.OFF_MAP) {
+				foundRightEdge = true;
+				mapRightEdge= scanForLand(toScan,+1,0).x;
+			} else{
+				if(myX>mapRightEdge) mapRightEdge = myX;
+			}
+		}
+		if(!foundBottomEdge) {
+			toScan = new MapLocation(myX,myY+SATRANGE);
+			if(myPlayer.myRC.senseTerrainTile(toScan)==TerrainTile.OFF_MAP) {
+				foundBottomEdge = true;
+				mapBottomEdge = scanForLand(toScan,0,+1).y;				
+			} else{
+				if(myY>mapBottomEdge) mapBottomEdge = myY;
+			}
+		}
+		if(!foundLeftEdge) {
+			toScan = new MapLocation(myX-SATRANGE,myY);
+			if(myPlayer.myRC.senseTerrainTile(toScan)==TerrainTile.OFF_MAP) {
+				foundLeftEdge = true;
+				mapLeftEdge = scanForLand(toScan,-1,0).x;
+			} else{
+				if(myX<mapLeftEdge) mapLeftEdge = myX;
+			}
+		}
 	}
+	
+	
+	
+	
+	/**
+	 * This function returns the first maplocation that is on-tile and not a void.
+	 * Used in conjunction with satelliteScanMapEdge in order to find the true map edge.
+	 * @param startLoc
+	 * @param dX
+	 * @param dY
+	 * @return MapLocation that is on tile.
+	 */
+	public MapLocation scanForLand(MapLocation startLoc, int dX, int dY) {
+		do {
+			startLoc = new MapLocation(startLoc.x + dX, startLoc.y + dY);
+		}while(myPlayer.myRC.senseTerrainTile(startLoc)==TerrainTile.OFF_MAP);
+		return startLoc;
+	}
+	
+	
+	/**
+	 * This function uses collected sensor data to estimate where the center of the map is.
+	 * @return
+	 */
+	public MapLocation estimateCenter(){
+		return null;
+	};
+	
+	
+	
 
-
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////////////
+	///////////// CALLBACKS /////////////////// /////////////////////////////////////////
+	
 	@Override
 	public void newComponentCallback(ComponentController[] components)
 	{
@@ -85,21 +204,31 @@ public class TestHeavyBehavior extends Behavior
 	@Override
 	public void newMessageCallback(MsgType t, Message msg)
 	{
-		if ( t == MsgType.MSG_SEND_NUM_FLYER )
+		if ( t == MsgType.MSG_SEND_NUM_HEAVY )
 		{
 			num = msg.ints[Messenger.firstData+1];
 			Utility.setIndicator(myPlayer, 2, "I'm heavy " + Integer.toString(num) + "!");
 		}
 	}
 	
-	public void onWakeupCallback(int lastActiveRound)
-	{
-		
-	}
+	public void onWakeupCallback(int lastActiveRound) {}
 	
-	public void onDamageCallback(double damageTaken)
+	public void onDamageCallback(double damageTaken)  {}
+
+	
+	
+	
+	
+	
+	
+	
+
+	
+	public String toString()
 	{
-		
+		return "TestHeavyBehavior";
 	}
+
+
 	
 }
