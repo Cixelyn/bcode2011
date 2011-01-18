9a7d99b894331c2618aea6d2ff4db9c8bc187e85
Cory Li <51144+Cixelyn@users.noreply.github.com>
2011-01-17 20:27:07 -0500

    more entries entered into jumptable

--- teams/suboptimal/JumpTable.java
+++ teams/suboptimal/JumpTable.java
@@ -6,7 +6,7 @@ import battlecode.common.*;
 
 
 /**
- * This function calculates jump vectors
+ * This class constructs JumpTables
  * @author Cory
  *
  */
@@ -20,6 +20,11 @@ public class JumpTable {
 	
 	private int idx;	//This is the current index we're checking
 	
+	/**
+	 * Instantiates a new JumpTable that calculates based on directions
+	 * @param loc
+	 * @param dir
+	 */
 	public JumpTable(MapLocation loc, Direction dir) {
 		myLoc=loc;
 		dx = dir.dx;
@@ -31,6 +36,12 @@ public class JumpTable {
 		idx = -1;
 	}
 	
+	
+	/**
+	 * This function returns the next best location to jump to.
+	 * If there are no more entries remaining in the table, the function returns null
+	 * @return MapLocation to jump to
+	 */
 	public MapLocation nextLoc() {
 	
 		//increment our index
@@ -43,9 +54,9 @@ public class JumpTable {
 			case 1:
 				return new MapLocation(myLoc.x + dx*2, myLoc.y + dy*3);
 			case 2:
-				return new MapLocation(myLoc.x + dx*1, myLoc.y + dy*3);
+				return new MapLocation(myLoc.x + dx  , myLoc.y + dy*3);
 			case 3:
-				return new MapLocation(myLoc.x + dx*3, myLoc.y + dy*1);
+				return new MapLocation(myLoc.x + dx*3, myLoc.y + dy  );
 			case 4:
 				return new MapLocation(myLoc.x + dx*2, myLoc.y + dx*2);
 			case 5:
@@ -53,7 +64,7 @@ public class JumpTable {
 			case 6:
 				return new MapLocation(myLoc.x       , myLoc.y + dx*3 );
 			case 7:
-				return new MapLocation(myLoc.x + dx*1, myLoc.y + dy*2 );
+				return new MapLocation(myLoc.x + dx  , myLoc.y + dy*2 );
 			case 8:
 				return new MapLocation(myLoc.x + dx*2, myLoc.y + dx*2 );
 			default:
@@ -73,6 +84,14 @@ public class JumpTable {
 					return new MapLocation(myLoc.x + 2,  myLoc.y+3*dy);
 				case 4:
 					return new MapLocation(myLoc.x - 2,  myLoc.y+3*dy);
+				case 5:
+					return new MapLocation(myLoc.x    ,  myLoc.y+3*dy);
+				case 6:
+					return new MapLocation(myLoc.x + 1,  myLoc.y+2*dy);
+				case 7:
+					return new MapLocation(myLoc.x - 1,  myLoc.y+2*dy);
+				case 8:
+					return new MapLocation(myLoc.x    ,  myLoc.y+2*dy);
 				default:
 					return null;
 				}
@@ -88,6 +107,14 @@ public class JumpTable {
 					return new MapLocation(myLoc.x+3*dx, myLoc.y + 2);
 				case 4:
 					return new MapLocation(myLoc.x+3*dx, myLoc.y - 2);
+				case 5:
+					return new MapLocation(myLoc.x+3*dx, myLoc.y    );
+				case 6:
+					return new MapLocation(myLoc.x+2*dx, myLoc.y + 1);
+				case 7:
+					return new MapLocation(myLoc.x+2*dx, myLoc.y - 1);
+				case 8:
+					return new MapLocation(myLoc.x+2*dx, myLoc.y    );
 				default: 
 					return null;
 				}	
