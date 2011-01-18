2357b1d8e721d831bb7c44400e75181095679a13
maxnelso <3494159+maxnelso@users.noreply.github.com>
2011-01-17 20:27:47 -0500

    hope this works !

--- teams/suboptimal/behaviors/ColossusBehavior.java
+++ teams/suboptimal/behaviors/ColossusBehavior.java
@@ -271,82 +271,6 @@ public class ColossusBehavior extends Behavior
 	
 	
 	
-	/**
-	 * Private class used to do the jump calculations
-	 * Eventually we should split into a separate file because of the overhead of inner classes
-	 * @author Cory
-	 *
-	 */
-	private class JumpTable {
-
-		private int dx; private int dy;
-		private boolean isDiagonal;
-		private boolean isVertical;
-		private MapLocation myLoc;
-		
-		public int NUM_CHOICES = 5;
-		
-		
-		public JumpTable(MapLocation loc, Direction dir) {
-			myLoc=loc;
-			dx = dir.dx;
-			dy = dir.dy;
-			isDiagonal = dir.isDiagonal();
-			isVertical = (dy!=0);
-		}
-		
-		public MapLocation nextLoc(int i) {
-			if(isDiagonal) {
-				switch(i) {
-				case 0:
-					return new MapLocation(myLoc.x + dx*3, myLoc.y + dy*2);
-				case 1:
-					return new MapLocation(myLoc.x + dx*2, myLoc.y + dy*3);
-				case 2:
-					return new MapLocation(myLoc.x + dx*1, myLoc.y + dy*3);
-				case 3:
-					return new MapLocation(myLoc.x + dx*3, myLoc.y + dy*1);
-				case 4:
-					return new MapLocation(myLoc.x + dx*2, myLoc.y + dx*2);
-				}
-			} else { //Not Diagonal
-				
-				if(isVertical) {
-					switch(i) {
-					case 0:
-						return new MapLocation(myLoc.x     , myLoc.y+4*dy);
-					case 1:
-						return new MapLocation(myLoc.x + 1 , myLoc.y+3*dy);
-					case 2:
-						return new MapLocation(myLoc.x - 1 , myLoc.y+3*dy);
-					case 3:
-						return new MapLocation(myLoc.x + 2,  myLoc.y+3*dy);
-					case 4:
-						return new MapLocation(myLoc.x - 2,  myLoc.y+3*dy);
-					}
-				} else {
-					switch(i) {
-					case 0:
-						return new MapLocation(myLoc.x+4*dx, myLoc.y    );
-					case 1:
-						return new MapLocation(myLoc.x+3*dx, myLoc.y + 1);
-					case 2:
-						return new MapLocation(myLoc.x+3*dx, myLoc.y - 1);
-					case 3:
-						return new MapLocation(myLoc.x+3*dx, myLoc.y + 2);
-					case 4:
-						return new MapLocation(myLoc.x+3*dx, myLoc.y - 2);
-					}	
-				}
-			}
-			
-			return null;
-			
-		}
-		
-	}
-	
-	
 	
 	/**
 	 * Attempts to make a jump in a particular direction.  If the jump fails
@@ -377,19 +301,25 @@ public class ColossusBehavior extends Behavior
 		if(!myPlayer.myJump.isActive()) {
 			
 			JumpTable jmp = new JumpTable(myPlayer.myRC.getLocation(),dir);
-
-			for(int i=jmp.NUM_CHOICES; --i>0;) {
-				MapLocation jmpLoc = jmp.nextLoc(jmp.NUM_CHOICES-i);
-				
-				if(canJump(jmpLoc)) {
+			MapLocation jmpLoc = jmp.nextLoc();
+			while (jmpLoc!=null) {
+				if (canJump(jmpLoc)) {
 					myPlayer.myJump.jump(jmpLoc);
 					return JMP_SUCCESS;
 				}
+				jmpLoc = jmp.nextLoc();
 			}
-			return JMP_NOT_POSSIBLE;
-		}else {
-			return JMP_NOT_YET;
+			
+			if (jmpLoc==null) {
+				return JMP_NOT_POSSIBLE;
+			}
+			if(canJump(jmpLoc)) {
+				myPlayer.myJump.jump(jmpLoc);
+				return JMP_SUCCESS;
+			}
 		}
+		return JMP_NOT_YET;
+		
 
 		
 	}
