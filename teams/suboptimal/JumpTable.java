d6331082c47daaf08477a669b6bb16b2363d18a5
Cory Li <51144+Cixelyn@users.noreply.github.com>
2011-01-18 18:31:33 -0500

    JumpTable refactor.  Horizontal + Vertical cases merged.

--- teams/suboptimal/JumpTable.java
+++ teams/suboptimal/JumpTable.java
@@ -6,7 +6,7 @@ import battlecode.common.*;
 
 
 /**
- * This class constructs JumpTables
+ * This class does the calculations necessary to figure out a good jump location
  * Table is fully self sufficient and can be used in doing calculations.
  * @author Cory
  *
@@ -90,89 +90,53 @@ public final class JumpTable {
 			default:
 				return null;
 			}
-		} else { //Not Diagonal
+		} else { //We need special code to deal with the orthogonals
+			
+			
+			int dirVec;
+			int zeroVec;
+			
+			switch(idx) {
+			case 0:
+				dirVec=4; zeroVec=0; break;
+			case 1:
+				dirVec=3; zeroVec=+1; break;
+			case 2:
+				dirVec=3; zeroVec=-1; break;
+			case 3:
+				dirVec=3; zeroVec=+2; break;
+			case 4:
+				dirVec=3; zeroVec=-2; break;
+			case 5:
+				dirVec=3; zeroVec=0; break;
+			case 6:
+				dirVec=2; zeroVec=+1; break;
+			case 7:
+				dirVec=2; zeroVec=-1; break;
+			case 8:
+				dirVec=2; zeroVec=0; break;
+				
+			//SKIPPING QUITE A FEW
+			case 9:
+				dirVec=0; zeroVec=+4; break;
+			case 10:
+				dirVec=0; zeroVec=-4; break;
+			case 11:
+				dirVec=0; zeroVec=+3; break;
+			case 12:
+				dirVec=0; zeroVec=-3; break;
+			default:
+				return null;
+			}
+			
 			
 			if(isVertical) {
-				switch(idx) {
-				case 0:
-					return new MapLocation(myLoc.x     , myLoc.y+4*dy);
-				case 1:
-					return new MapLocation(myLoc.x + 1 , myLoc.y+3*dy);
-				case 2:
-					return new MapLocation(myLoc.x - 1 , myLoc.y+3*dy);
-				case 3:
-					return new MapLocation(myLoc.x + 2,  myLoc.y+3*dy);
-				case 4:
-					return new MapLocation(myLoc.x - 2,  myLoc.y+3*dy);
-				case 5:
-					return new MapLocation(myLoc.x    ,  myLoc.y+3*dy);
-					
-				//End of the range 3 squares
-				case 6:
-					return new MapLocation(myLoc.x + 1,  myLoc.y+2*dy);
-				case 7:
-					return new MapLocation(myLoc.x - 1,  myLoc.y+2*dy);
-				case 8:
-					return new MapLocation(myLoc.x    ,  myLoc.y+2*dy);
-				
-				
-				//SKIPPING QUITE A FEW
-				case 9:
-					return new MapLocation(myLoc.x + 4, myLoc.y      );
-				case 10:
-					return new MapLocation(myLoc.x - 4, myLoc.y      );
-				case 11:
-					return new MapLocation(myLoc.x + 3, myLoc.y      );
-				case 12:
-					return new MapLocation(myLoc.x - 3, myLoc.y      );
-					
-					
-				
-				
-				default:
-					return null;
-				}
-			} else {
-				switch(idx) {
-				case 0:
-					return new MapLocation(myLoc.x+4*dx, myLoc.y    );
-				case 1:
-					return new MapLocation(myLoc.x+3*dx, myLoc.y + 1);
-				case 2:
-					return new MapLocation(myLoc.x+3*dx, myLoc.y - 1);
-				case 3:
-					return new MapLocation(myLoc.x+3*dx, myLoc.y + 2);
-				case 4:
-					return new MapLocation(myLoc.x+3*dx, myLoc.y - 2);
-				case 5:
-					return new MapLocation(myLoc.x+3*dx, myLoc.y    );
-				case 6:
-					return new MapLocation(myLoc.x+2*dx, myLoc.y + 1);
-				case 7:
-					return new MapLocation(myLoc.x+2*dx, myLoc.y - 1);
-				case 8:
-					return new MapLocation(myLoc.x+2*dx, myLoc.y    );
-
-				
-				//SKIPPING QUITE A FEW
-				case 9:
-					return new MapLocation(myLoc.x    , myLoc.y + 4 );
-				case 10:
-					return new MapLocation(myLoc.x    , myLoc.y - 4 );
-				case 11:
-					return new MapLocation(myLoc.x    , myLoc.y + 3 );
-				case 12:
-					return new MapLocation(myLoc.x    , myLoc.y - 3 );
-					
-				
-				
-				
-				default: 
-					return null;
-				}	
+				return new MapLocation(myLoc.x+zeroVec, myLoc.y+dirVec*dy);
+			} else{
+				return new MapLocation(myLoc.x+dirVec*dx, myLoc.y+zeroVec);
 			}
 		}
-
+		
 		
 	}
 	
