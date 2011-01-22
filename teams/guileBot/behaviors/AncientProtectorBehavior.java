7b7aa7db94e5b100a7f639f6a6c356b1b43cb476
Justin Venezuela <jven@jvenezue.la>
2011-01-22 16:05:15 -0500

    Added ascii art of photon cannon

--- teams/guileBot/behaviors/AncientProtectorBehavior.java
+++ teams/guileBot/behaviors/AncientProtectorBehavior.java
@@ -8,8 +8,49 @@ import battlecode.common.*;;
 /**
  * 
  * 
+ * <pre>
  * 
- * WC3 is the best, how's thats for ASCII art?
+ *                                                                                 
+ *     Since WC3 is essentially a dead game, there exists no community from which
+ *     a wire-frame picture of an "Ancient Protector" (whatever that is) can be used
+ *     for ASCII art. We have therefore taken the liberty of replacing said "tower"
+ *     with art of the awesomer and more WELL KNOWN photon cannon.
+ *     
+ *                                                                       
+ *                                                                               
+ *                                          ..M  .                               
+ *                       . MM              ,M ,. MM,,                            
+ *                     MN.....MM.M.        MM      . MM                          
+ *                 .MM        .M    .......    MM.  .M.                          
+ *                M   .M     .MMM   M.MMMM .      .M. MM.          ..            
+ *                 M   .M MM . .MM   M... M    .MM. MM .. M..      ..            
+ *                   M.  N   ...MNN MM   .NMMM.  .MMN .     MM   M,  M.          
+ *                .M..N..M..MNM . .  M , .N .M.MM.   ,MM   MM     M.  .M.        
+ *        .MMM    M    .MMM..    MMM. ..  . M..MM      . ..M.M     MMM .,N       
+ *      M  M     M ..M.     , M,   .NMNMMM..M  .   MM .  ..M. .MM.     MM.N      
+ *     M  M       MM     MMM    ..M M.  .M.   MM.     M.   M             M       
+ *    .,.MNMM.   MMM  M M.   .MM.   M.   . M   ..MN     M   .M         MM.       
+ *    MM      MMM M .M   MNMM     . M.     M.MM. . MM    N..  .M. .  MM.         
+ *   .M         M M M    .M      .M M      M.MM  N...M   .M.  .M.  .MN  ..       
+ *   ..MM       M. M     ..      M.  M    M.  .      M.  .    N..  ...MMM        
+ *        .M  ..MMMM.     .                    M     M   .M...M.     M     .     
+ *           MM.  .MM.   .M.     MN .        MM    .,M   .M.. N. MMMM     .M     
+ *        .M .       M     M.       MDMMMNM..       M.  .M. ..M     .M     M     
+ *       NM M..  ..MM...     M ..                .M   . M     .M      M   .M     
+ *    .,M   .M .MM    MM      ,MM.       ..    .MM . MMM        M  .....M.M:     
+ *    .M      M..    .M..M     NMM..MMM.... MMM   MN ,  .M.    .MNM.....MM.      
+ *    .M     M..      N.  .MM.M. .   MN  .      ..M      .M. .M.. . ... ..       
+ *       M  M.        M.    .M        MM...... MMM. M .MMM MM.                   
+ *       .MM  DMNMMM ..MM.. MMMM.   .N  M.....  .M   M..    M.                   
+ *        . .          .  MM  . ..MMM...M       .M   M       M                   
+ *                         N        M  .M, .  . M,    M,      .                  
+ *                       .M        M  ..M ......  N.         .M                  
+ *                       .MM       M..M..            M M .MMM..                  
+ *                           M   ..MM                                            
+ *                              ...                                              
+ *                                                                           
+ * 
+ * </pre>
  * 
  *                                           .                                    
  *
